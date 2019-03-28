5ada8e633940804b52d0ec18784ad74ad3f37e00
denys <denys.potapov@gmail.com>
2019-03-28 23:54:05 +0200

    mnouse fix

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # js1k
 js1k 2019 submission
 
-Navigate with mouse to find bigger stars
+Navigate with mouse with left button down to find bigger stars
 
 This is a golfed version of Star Nest by Pablo Roman Andrioli (https://www.shadertoy.com/view/XlfGRj)
--- small.glsl
+++ small.glsl
@@ -1,18 +1,14 @@
 precision mediump float;
-uniform float t,a;
-varying lowp vec4 fragCoord;
-uniform vec2 iResolution;
-uniform vec4 iMouse;
+uniform float t,a,x,y;
 
 void main() {
-  //get coords and direction
-  vec2 uv=gl_FragCoord.xy/iResolution.xy-.5;
-  uv.y*=a;
-  vec3 dir=vec3(uv,1.);
+  //get direction
+  vec3 dir=vec3(gl_FragCoord.xy/640.-.5,1.);
+  dir.y*=a;
 
   //mouse rotation
-  float a1=.5+iMouse.x/iResolution.x*2.;
-  float a2=.8+iMouse.y/iResolution.y*2.;
+  float a1=.5+x;
+  float a2=.5+y;
   mat2 rot1=mat2(cos(a1),sin(a1),-sin(a1),cos(a1));
   mat2 rot2=mat2(cos(a2),sin(a2),-sin(a2),cos(a2));
   dir.xz*=rot1;
@@ -40,16 +36,14 @@ void main() {
       pa=length(p);
     }
     // #define darkmatter 0.300
-    float dm=max(0.,.3-a*a*.001); //dark matter
-    
-    if (r>6) fade*=1.-dm; // dark matter, don't render near
+
+    if (r>6) fade*=1.-max(0.,.3-a*a*.001);; // dark matter, don't render near
     //v+=vec3(dm,dm*.5,0.);
-    v+=fade;
-    a*=a*a; // add contrast
+
     // v+=vec3(s,s*s,s*s*s*s)*a*brightness*fade;
     // #define brightness 0.0015
 
-    v+=vec3(s,s*s,s*s*s*s)*a*.0015*fade; // coloring based on distance
+    v+=fade + vec3(s,s*s,s*s*s*s)*a*a*a.0015*fade; // coloring based on distance
     fade*=.73; // distance fading #define distfading 0.730
     s+=0.1; // #define stepsize 0.1
   }
